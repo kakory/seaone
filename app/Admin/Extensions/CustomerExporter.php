33f0dd5d5bbe401752a3ee72df9dba25f716d74a
Kururhythm <benikuroge@gmail.com>
2021-05-24 06:38:43 +0000

    adviser

--- app/Admin/Extensions/CustomerExporter.php
+++ app/Admin/Extensions/CustomerExporter.php
@@ -2,11 +2,31 @@
 
 namespace App\Admin\Extensions;
 
-use Encore\Admin\Grid\Exporters\ExcelExporter; 
+use Encore\Admin\Grid\Exporters\ExcelExporter;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class CustomerExporter extends ExcelExporter
+class CustomerExporter extends ExcelExporter implements WithMapping
 {
     protected $fileName = 'Customers.xlsx';
 
-    protected $headings = ['id', 'name', 'phone_number', 'company_name', 'photo', 'remark', 'created_at', 'updated_at'];
+    protected $columns = [
+        'id'=>'id',
+        'name'=>'name',
+        'phone_number'=>'phone_number',
+        'company_name'=>'company_name',
+        'adviser_id'=>'adviser_id',
+        'remark'=>'remark',
+    ];
+
+    public function map($customer) : array
+    {
+        return [
+            $customer->id,
+            $customer->name,
+            $customer->phone_number,
+            $customer->company_name,
+            data_get($customer, 'adviser.name'),
+            $customer->remark,
+        ];
+    }
 }
--- app/Admin/Extensions/SeminarCustomerExporter.php
+++ app/Admin/Extensions/SeminarCustomerExporter.php
@@ -2,7 +2,7 @@
 
 namespace App\Admin\Extensions;
 
-use Encore\Admin\Grid\Exporters\ExcelExporter; 
+use Encore\Admin\Grid\Exporters\ExcelExporter;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
 class SeminarCustomerExporter extends ExcelExporter implements WithMapping
@@ -12,27 +12,21 @@ class SeminarCustomerExporter extends ExcelExporter implements WithMapping
     protected $columns = [
         'id'=>'id',
         'seminar_id'=>'seminar_id',
-        'seminar.name'=>'seminar.name',
         'customer_id'=>'customer_id',
-        'customer.name'=>'customer.name',
-        'customer.phone_number'=>'customer.phone_number',
-        'customer.company_name'=>'customer.company_name',
-        'created_at'=>'created_at',
-        'status'=>'status',
+        'customer_id'=>'customer_id',
+        'customer_id'=>'customer_id',
+        'customer_id'=>'customer_id',
     ];
 
     public function map($seminarCustomer) : array
     {
         return [
             $seminarCustomer->id,
-            $seminarCustomer->seminar_id,
             data_get($seminarCustomer, 'seminar.name'),
-            $seminarCustomer->customer_id,
             data_get($seminarCustomer, 'customer.name'),
             data_get($seminarCustomer, 'customer.phone_number'),
             data_get($seminarCustomer, 'customer.company_name'),
-            $seminarCustomer->created_at,
-            $seminarCustomer->status,
+            data_get($seminarCustomer, 'customer.adviser.name'),
         ];
     }
 }
